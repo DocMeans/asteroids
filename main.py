16dbac25cede2eb748db5bc61c9878b6a292fc68
DocMeans <docmeans071@gmail.com>
2025-05-18 14:25:39 -0400

    finished base game

--- main.py
+++ main.py
@@ -3,16 +3,13 @@
 # throughout this file
 import sys
 import pygame
-from constants import *
+from constants import WHITE, BLACK, SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     # screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -22,14 +19,18 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     # Containers
-    Player.containers = (updatable, drawable)
+    
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
+    Shot.containers = (shots, updatable, drawable)
 
+    Player.containers = (updatable, drawable)
 
+    # center the player in the screen
     player = Player((SCREEN_WIDTH / 2), SCREEN_HEIGHT / 2)
 
     dt = 0
@@ -38,15 +39,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, BLACK)
 
         updatable.update(dt)
+        
 
-        # CHeck for collision with the player object
+        # Check for collision with the player object
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         # Draw the objects to the screen
         for obj in drawable:
