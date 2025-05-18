21d7a6c18a35473a538598641eb2d31546b7ab83
DocMeans <docmeans071@gmail.com>
2025-05-18 13:09:09 -0400

    Added objects (player, asteroids) and collision

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-
     pygame.init()
+    # screen size
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    frames = pygame.time.Clock()
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+
+    player = Player((SCREEN_WIDTH / 2), SCREEN_HEIGHT / 2)
+
+    dt = 0
+    # game loop
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, BLACK)
-        #pygame.display.update()
+
+        updatable.update(dt)
+
+        # CHeck for collision with the player object
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        # Draw the objects to the screen
+        for obj in drawable:
+            obj.draw(screen)       
+        
+        
         pygame.display.flip()
+        
+        dt = frames.tick(60) / 1000
 
 
 if __name__ == "__main__":
